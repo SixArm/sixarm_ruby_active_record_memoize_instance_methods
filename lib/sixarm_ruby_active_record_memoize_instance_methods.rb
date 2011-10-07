59c0e810085189a6424c180bfb0af4f6a72bb9da
SixArm <sixarm@sixarm.com>
2011-10-06 21:25:10 -0700

    Updates for gem publishing

--- lib/sixarm_ruby_active_record_memoize_instance_methods.rb
+++ lib/sixarm_ruby_active_record_memoize_instance_methods.rb
@@ -1,19 +1,7 @@
 # -*- coding: utf-8 -*-
-
 =begin rdoc
-
-= SixArm.com » Ruby » ActiveRecord Memoize Instance Methods
-
-Author:: Joel Parker Henderson, joelparkerhenderson@gmail.com
-Copyright:: Copyright (c) 2009-2011 Joel Parker Henderson
-License:: See LICENSE.txt file
-
-ActiveRecord extenstions: ActiveSupport::Memoizable for instance methods.
-
-See http://www.railway.at/articles/2008/09/20/a-guide-to-memoization
-
+Please see README.rdoc
 =end
-
 
 require 'activerecord'
 require 'activesupport'
---
+++ test/sixarm_ruby_active_record_memoize_instance_methods_test.rb
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+require 'test/unit'
+require 'simplecov'
+SimpleCov.start
+require ' sixarm_ruby_active_record_memoize_instance_methods'
+
+class Testing < Test::Unit::TestCase
+end
